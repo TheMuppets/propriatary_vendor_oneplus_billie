62e7a654ba2095513e706c2de292bb02ed42a55a
Saalim Quadri <danascape@gmail.com>
2024-08-14 16:52:30 +0530

    billie: Add firmware from OOS 11 RKQ1.201217.002/2311081107
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- Android.mk
+++ Android.mk
@@ -6,4 +6,21 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),billie)
 
+$(call add-radio-file-sha1-checked,radio/abl.img,0ae500c5f24f1a2b77bb8b774908364766965b88)
+$(call add-radio-file-sha1-checked,radio/aop.img,fd1513c6930458c600934738bd19357b00e83681)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,0d46dfab1e69459f427f52be08ebf86091aaaa76)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,ff295616270ac7ca8ad2e50f9098e6a71777df31)
+$(call add-radio-file-sha1-checked,radio/dsp.img,a7b3320234afefe26553a89b7456b382fec5ce9c)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,15664dda01e6988bfd57a5641d6f988aa8d884cd)
+$(call add-radio-file-sha1-checked,radio/hyp.img,8b400139eb022bdae6e1648483ebf48a4821446f)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,08c26ccfb84c5221ea5431c0101973f6e79a0e29)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,2888f9782703b64721ff5d7c5fe7c6d6223e14e8)
+$(call add-radio-file-sha1-checked,radio/modem.img,684665c108628ab3c6aa34d5f00300d9e5840310)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,6b877fc1b6588338e4d94e405fc28f5a60198588)
+$(call add-radio-file-sha1-checked,radio/storsec.img,4a919c24417f634081100c369629cecc33ba55ea)
+$(call add-radio-file-sha1-checked,radio/tz.img,790f803dc41f27c2c7fb0eb2573e56e6e0941180)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,f832bbac1c4dc9b3fa42ee3425e3e894f0fca688)
+$(call add-radio-file-sha1-checked,radio/xbl.img,96d9626ab7471c706f467dd9ccd24e884fce8504)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,93f9ee85376ee3a9dc08010dcb12f121f855599f)
+
 endif
